c46103cfab0373686b85ab920ff3b6af3de200c5
lerefugefm-design <lerefugefm@gmail.com>
2026-09-23 01:56:05 +0200

    Add Gym Leader career core: include/gym_leader.h

---
+++ include/gym_leader.h
@@ -0,0 +1,30 @@
+#ifndef GUARD_GYM_LEADER_H
+#define GUARD_GYM_LEADER_H
+
+#include "global.h"
+
+void GymLeader_InitNewCareer(void);
+void GymLeader_RecordDefenseResult(bool8 won);
+u8 GymLeader_GetRecommendedLevel(void);
+u8 GymLeader_GetMaxPartySize(void);
+u16 GymLeader_GetStarterSpecies(u8 specialty);
+u8 GymLeader_SpecialtyToEngineType(u8 specialty);
+bool8 GymLeader_SpeciesMatchesSpecialty(u16 species, u8 specialty);
+bool8 GymLeader_IsCurrentPartyLegal(void);
+u8 GymLeader_CountPartySpecialists(void);
+u16 GymLeader_GetUpgradeCost(u8 upgradeIndex);
+bool8 GymLeader_PurchaseUpgrade(u8 upgradeIndex);
+
+void Special_GymLeader_InitCareer(void);
+void Special_GymLeader_SetSpecialty(void);
+void Special_GymLeader_GetStarter(void);
+void Special_GymLeader_GiveStarter(void);
+void Special_GymLeader_RecordWin(void);
+void Special_GymLeader_RecordLoss(void);
+void Special_GymLeader_GetRecommendedLevel(void);
+void Special_GymLeader_CheckPartyRules(void);
+void Special_GymLeader_AdvanceWeek(void);
+void Special_GymLeader_GetUpgradeCost(void);
+void Special_GymLeader_PurchaseUpgrade(void);
+
+#endif // GUARD_GYM_LEADER_H
